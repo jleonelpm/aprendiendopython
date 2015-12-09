ff00f1052b0185e9fef5957ef154f4a28f472595
Leonel <jleonelpm@hotmail.com>
2015-12-09 01:45:05 -0600

    Ejemplos básicos con cadenas

---
+++ cadenas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+trabalenguas = "pepe pecas pica papas con un pico con un pico pica papas pepe pecas"
+print "Mostrando la longitud de una cadena con len"
+print len (trabalenguas)
+print "Listando los métodos disponibles para un objeto con dir"
+dir (trabalenguas) #Funciona únicamente en la consola, pero es bastante útil
+print trabalenguas
+print "Conviertiendo a mayúsculas con el método upper"
+print trabalenguas.upper()
+print "Primera letra en mayúsculas con el método capitalize"
+print trabalenguas.capitalize()
